885a9c71452deaa9af4ac7089c0676e7a0033606
Brallen <brett.case97@gmail.com>
2018-06-01 13:34:56 -0700

    server and client send basic information on control port

--- project2/ftclient.py
+++ project2/ftclient.py
@@ -3,45 +3,54 @@
 from socket import *
 from sys import argv
 
-if len(argv) > 1: #if a port was specified use that one
-	serverPort = int(argv[1])
-else:
-   	serverPort = 42069 #otherwise use this one
+def clientConnector(hostname, serverPort, command, filename, dataPort):
+	#set up the client to connect to the server
+	clientSocket = socket(AF_INET, SOCK_STREAM)
+	clientSocket.connect((hostname, serverPort))
+	clientSocket.send(str(dataPort) + '@')
+	bit = clientSocket.recv(1)
+	clientSocket.send(str(command) + '@')
+	print(bit)
+	#clientSocket.close()
 
-#get the username and make sure its not too long
-username = raw_input('Enter username: ')
-if len(username) > 10:
-	print 'name is too long' 
-	exit(1)
 
-#set up the socket to listen
-serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind(('',serverPort))
-serverSocket.listen(1)
-print 'The server is ready on port %d' % serverPort
 
-while(1): #start listening for connections
-	connectionSocket, addr = serverSocket.accept() #block and wait for connection
-	print 'connection established' #let user know a connection has been made
-	recText = ""
-	while 1: #while we have not send or received a \quit do stuff
-		recText = connectionSocket.recv(550) #receive message
-		if recText.find("\quit") == -1: #check to see if it is a disconnect message
-			#if its not do all this 
-			print recText
-			
-			#creat and send message to client
-			sendText = raw_input(username + ': ')
-			if len(sendText) > 500:
-				sendText = sendText[:500]
-			connectionSocket.send(username + ': ' + sendText)
-			#look to see if we are sending a disconnect
-			if sendText.find("\quit") != -1:
-				print 'connection ended'
-				break #if we are then break out of the loop to close connection
-		else:
-			#if it was a disconnect message then break out the loop
-			print 'client has left the chat' 
+def dataServer(dataPort):
+	#set up the socket to listen
+	serverSocket = socket(AF_INET, SOCK_STREAM)
+	serverSocket.bind(('',dataPort))
+	serverSocket.listen(1)
+	print 'The server is ready on port %d' % dataPort
+
+	while(1): #start listening for connections
+		connectionSocket, addr = serverSocket.accept() #block and wait for connection
+		print 'connection established' #let user know a connection has been made
+		recText = ""
+		while 1: #while we have not send or received a \quit do stuff
+			recText = connectionSocket.recv(550) #receive message
+			if recText.find("\quit") == -1: #check to see if it is a disconnect message
+				#if its not do all this 
+				print recText
 			break
-			
-	connectionSocket.close() #close the connection and then go back to listening
+				
+		connectionSocket.close() #close the connection and then go back to listening
+
+if __name__ == "__main__":
+	#args: <SERVER_HOST>, <SERVER_PORT>, <COMMAND>, if -g:(<FILENAME>), <DATA_PORT>
+	if len(argv) == 5 and argv[3] == '-l': #the number of commands for -l
+		hostname = argv[1]
+		serverPort = int(argv[2])
+		command = argv[3] #not turned to an int because its the command
+		dataPort = int(argv[4])
+		filename = '!!!' #mark that there is no filename and want -l
+	elif len(argv) == 6 and argv[3] == '-g': #the number of commands for -g
+		hostname = argv[1]
+		serverPort = int(argv[2])
+		command = argv[3] #not turned to an int because its the command
+		filename = argv[4] #not an int cause its the filename
+		dataPort = int(argv[5])
+	else:
+		print 'Wrong arguments supplied'
+		exit(1)
+
+	clientConnector(hostname, serverPort, command, filename, dataPort) #setup initial connection to server
--- project2/ftserver.c
+++ project2/ftserver.c
@@ -5,8 +5,9 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 
-void encryptString(char *, char *, char *);
+void sendDirectory(int);
 void error(const char *msg){
 	perror(msg);
 	exit(1);
@@ -15,7 +16,7 @@ void error(const char *msg){
 int main(int argc, char *argv[]){
 	int listenSocketFD, establishedConnectionFD, portNumber, charsRead, spawnPid, childExitMethod, j, i;
 	socklen_t sizeOfClientInfo;
-	char buffer[70005], message[70005], file[70005], dataPort[7];
+	char buffer[70005], message[70005], file[70005], dataPort[7], command[3];
 	struct sockaddr_in serverAddress, clientAddress;
 
 	if (argc != 2){
@@ -42,52 +43,52 @@ int main(int argc, char *argv[]){
 
 	//don't kill the server after just one connection
 	while (1){ //could change this later so server doesn't have to be killed with ^C
+
+		//initialize variables to be nothing
+		memset(dataPort, '\0', 7);
+		memset(command, '\0', 3);
 		// Accept a connection, blocking if one is not available until one connects
 		sizeOfClientInfo = sizeof(clientAddress); // Get the size of the address for the client that will connect
 		establishedConnectionFD = accept(listenSocketFD, (struct sockaddr *)&clientAddress, &sizeOfClientInfo); // Accept
+		printf("client IP address is: %s\n", inet_ntoa(clientAddress.sin_addr));
 		if (establishedConnectionFD < 0)
 			error("ERROR on accept");
-		//fork when get a connection
-		spawnPid = fork();
-		switch (spawnPid){
-		case -1: //error case
-			perror("Hull Breach!");
-			exit(1);
-			break;
-		case 0: // child case (this is where all communication happens)
-			while (i){ //keep running till have sent and received everything
-				// Get the file from the client and display it
-				memset(buffer, '\0', 70005);
-				while (strstr(file, "@") == NULL){ //look for @ to know when client port number has ended
-					charsRead = recv(establishedConnectionFD, buffer, 1, 0); // Read the client's port from the socket
-					strcat(dataPort, buffer);
-				}	
-				memset(buffer, '\0', 70005);
-				while (strstr(file, "\n") == NULL){
-					charsRead = recv(establishedConnectionFD, buffer, 1, 0); // Read the client's message from the socket
-					strcat(dataPort, buffer);
-				}				
-				if (charsRead < 0)
-					error("ERROR reading from socket");
 
-				//see what we got
-				if (strncmp(buffer, "-l", 2) == 0){
-					//send the list of the directory contents
-					//charsRead = send(establishedConnectionFD, "wrong server@", 13, 0);
-					sendDirectory(atoi(dataPort));
-					i = 0; //once done with that end the loop
-				}else if(strncmp(buffer, "-g", 2) == 0){
-					//send the file that the client wanted on different port
-					//charsRead = send(establishedConnectionFD, "wrong server@", 13, 0);
-					
-					i = 0; //once done with that end the loop
-				}
-			}
-			close(establishedConnectionFD); // Close the existing socket which is connected to the client
-			exit(0);
-		default: //parent, just hang out and clean up child
-			waitpid(spawnPid, &childExitMethod, 0);
+		printf("IN CHILD\n");
+		// Get the dataPort from the client and display it
+		memset(buffer, '\0', 70005);
+		while (strstr(dataPort, "@") == NULL){ //look for @ to know when client port number has ended
+			charsRead = recv(establishedConnectionFD, buffer, 1, 0); // Read the client's port from the socket
+			strcat(dataPort, buffer);
+		}	
+		printf("dataPort: %d\n", atoi(dataPort));
+		charsRead = send(establishedConnectionFD, "1", 1, 0);
+
+		//get the command from the client and display it
+		memset(buffer, '\0', 70005);
+		while (strstr(command, "@") == NULL){ //look for @ to know when client port number has ended
+			charsRead = recv(establishedConnectionFD, buffer, 1, 0); // Read the client's port from the socket
+			strcat(command, buffer);
 		}
+		command[2] = '\0'; //set the last bit to a null terminator instead of @
+		printf("command: %s\n", command);
+		charsRead = send(establishedConnectionFD, "1", 1, 0);
+
+		//see what we got
+		if (strncmp(buffer, "-l", 2) == 0){
+			//send the list of the directory contents
+			//charsRead = send(establishedConnectionFD, "wrong server@", 13, 0);
+			sendDirectory(strtol(dataPort, NULL, 10));
+			i = 0; //once done with that end the loop
+		}else if(strncmp(buffer, "-g", 2) == 0){
+			//send the file that the client wanted on different port
+			//charsRead = send(establishedConnectionFD, "wrong server@", 13, 0);
+			
+			i = 0; //once done with that end the loop
+		}
+		close(establishedConnectionFD); // Close the existing socket which is connected to the client
+		printf("Connection with %s closed\n", inet_ntoa(clientAddress.sin_addr));
+
 	}
 
 	close(listenSocketFD); // Close the listening socket
